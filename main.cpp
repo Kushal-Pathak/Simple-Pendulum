13053267a643d8d32130e29e5e431ce2d8186426
Kushal Pathak <84308717+Kushal-Pathak@users.noreply.github.com>
2023-03-13 23:16:34 +0545

    updated code for angle

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #define height 900
 #define width 1200
+#define pi 3.14159f
 
 class Pendulum {
 public:
@@ -12,7 +13,7 @@ public:
 	Pendulum() {
 		anchor_pos = sf::Vector2f(width / 2, 20);
 		length = 500;
-		angle = 45;
+		angle = pi/4;
 		bob_pos.x = length * sin(angle) + anchor_pos.x;
 		bob_pos.y = length * cos(angle) + anchor_pos.y;
 		bob.setRadius(30);
@@ -26,10 +27,10 @@ public:
 	Pendulum(sf::Vector2f a_pos, float len, float ang) {
 		anchor_pos = a_pos;
 		length = len;
-		angle = ang;
+		angle = ang * pi / 180.f;
 		bob_pos.x = length * sin(angle) + anchor_pos.x;
 		bob_pos.y = length * cos(angle) + anchor_pos.y;
-		bob.setRadius(50);
+		bob.setRadius(30);
 		anchor.setFillColor(sf::Color::Blue);
 		bob.setFillColor(sf::Color::Red);
 		update_anchor();
@@ -63,6 +64,8 @@ public:
 int main() {
 	sf::RenderWindow window(sf::VideoMode(width, height), "My Window");
 	Pendulum pendulum;
+	Pendulum p1(sf::Vector2f(width / 3, 20), 500, 60);
+	Pendulum p2(sf::Vector2f(width * 5 / 6, 20), 500, -60);
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
@@ -72,10 +75,24 @@ int main() {
 		}
 		Sleep(30);
 		window.clear(sf::Color::Black);
+
 		window.draw(pendulum.line, 2, sf::Lines);
 		window.draw(pendulum.anchor);
 		window.draw(pendulum.bob);
+
+		window.draw(p1.line, 2, sf::Lines);
+		window.draw(p1.anchor);
+		window.draw(p1.bob);
+
+		window.draw(p2.line, 2, sf::Lines);
+		window.draw(p2.anchor);
+		window.draw(p2.bob);
+		
 		window.display();
 		pendulum.update();
+
+		p1.update();
+
+		p2.update();
 	}
 }
